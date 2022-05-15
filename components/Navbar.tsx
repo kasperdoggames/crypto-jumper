07c2eb49ca79128cb8cc7ccd156c1751f7311107
Ian Griggs <iangriggs@hotmail.com>
2022-05-15 20:17:13 +0100

    added a static link to a an NFT image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const user = {
   name: "Tom Cook",
   email: "tom@example.com",
   imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "https://bafybeifme5ivs3kv3nd5wcdsbdf52dbg3nysoty62bxanxj6z36kvlcy3y.ipfs.dweb.link/2.png",
 };
 const navigation = [
   { name: "About", href: "about", current: true },
